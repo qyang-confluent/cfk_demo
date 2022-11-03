54080c98b04199abed3ba7bd805b2e23f25f3fad
Ubuntu <ubuntu@ip-10-0-1-54.ec2.internal>
2022-11-03 19:39:25 +0000

    add argocd

---
+++ argocd/install.sh
@@ -0,0 +1,4 @@
+#kubectl apply -n qyang -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+kubectl apply -n qyang -f install.yaml
+#kubectl apply -n qyang -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/namespace-install.yaml
+
---
+++ argocd/port.sh
@@ -0,0 +1,1 @@
+kubectl port-forward svc/argocd-server -n qyang 8080:443 --address='0.0.0.0'
